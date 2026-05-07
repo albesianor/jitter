8f02c0946a44df35b84dabe1a4517ee4f10fd2b2
Roberto Albesiano <roberto.albesiano@gmail.com>
2026-05-07 11:44:34 -0400

    feat: add scoring module

---
+++ scoring.py
@@ -0,0 +1,26 @@
+from torch.nn.functional import cosine_similarity
+import torch
+
+class CosScorer:
+    """
+    A cosine-similarity scorer class.
+    """
+
+    def __init__(self, centroid: torch.Tensor):
+        """
+        Args:
+            centroid (torch.Tensor): The centroid for cosine-similarity scoring.
+        """
+        self.centroid = centroid
+
+    def __call__(self, tensor: torch.Tensor) -> float:
+        """
+        Args:
+            tensor (torch.Tensor): The tensor to score.
+
+        Returns:
+            float: The cosine-similarity score.
+        """
+        return float(cosine_similarity(tensor, self.centroid, dim=1))
+
+    
