a831191af9b6af9b6a4ec529bff4b5206d4b09e4
Roberto Albesiano <roberto.albesiano@gmail.com>
2026-05-20 10:24:26 -0400

    fix: wrap feed parsing in try block

--- sourcing.py
+++ sourcing.py
@@ -19,17 +19,23 @@ def from_urls(urls: list) -> list:
     parsed_feed = []
 
     for url in urls:
-        feed = feedparser.parse(url)
+        try:
+            feed = feedparser.parse(url)
+        except:
+            print("Unable to parse feed", url)
 
         for entry in feed.entries:
-            parsed_entry = {
-                "title": entry.title,
-                "description": entry.description,
-                "timestamp": entry.published_parsed,
-                # "source": feed.feed.title
-            }
+            try:
+                parsed_entry = {
+                    "title": entry.title,
+                    "description": entry.description,
+                    "timestamp": entry.published_parsed,
+                    # "source": feed.feed.title
+                }
+                parsed_feed.append(parsed_entry)
+            except:
+                print("Skipping", entry)
 
-            parsed_feed.append(parsed_entry)
 
     return pd.DataFrame(parsed_feed).drop_duplicates(subset="title", inplace=False).dropna(inplace=False)
 
