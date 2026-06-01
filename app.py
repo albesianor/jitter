55eded8f32c69a97754558810575de0807786155
Roberto Albesiano <roberto.albesiano@gmail.com>
2026-06-01 15:59:01 -0400

    feat: add status and pydantic response models

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from contextlib import asynccontextmanager
 from jitter import JitterEvaluator, get_headlines
 import pandas as pd
-import ast
+import ast, datetime
 
-engine = None
+
+class Status(BaseModel):
+    last_trained: datetime.datetime
+    last_updated: datetime.datetime
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global engine
+    global status
 
     print("Loading model.")
     engine = JitterEvaluator()
@@ -19,11 +24,16 @@ async def lifespan(app: FastAPI):
     training["embedding"] = training.embedding.apply(ast.literal_eval)
 
     engine.train(training)
+    train_time = datetime.datetime.now()
 
     print("Begin predictions.")
     urls = pd.read_csv("sources.csv").url
     headlines = get_headlines(urls, date="today")
     engine.process_headlines(headlines)
+
+    update_time = datetime.datetime.now()
+
+    status = Status(last_trained=train_time, last_updated=update_time)
 
     print("Engine ready.")
 
@@ -35,13 +45,21 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
+class Summary(BaseModel):
+    status: Status
+    mean: float
+    std: float
+
+
 @app.get("/summary/")
-async def summary():
-    return {
-        "mean": engine.mean,
-        "std": engine.std,
-    }
+async def summary() -> Summary:
+    return Summary(status=status, mean=engine.mean, std=engine.std)
+
+
+class Distribution(BaseModel):
+    status: Status
+    distribution: list[float]
 
 @app.get("/distribution/")
-async def distribution():
-    return list(engine.distribution)
+async def distribution() -> Distribution:
+    return Distribution(status=status, distribution=list(engine.distribution))
